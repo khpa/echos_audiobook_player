0ecfbc8fa673c573a253bedb2662a6f479f7479f
MichaBrugger <michabrugger14@gmail.com>
2021-10-16 15:13:10 +0200

    cleaned up permission request handling using react-native-permissions

--- src/data/permissions/handlePermissions.ts
+++ src/data/permissions/handlePermissions.ts
@@ -2,13 +2,16 @@ import {Platform} from 'react-native';
 import {PERMISSIONS} from 'react-native-permissions';
 import {handleAndroidPermissions} from './handleAndroidPermissions';
 
-// distribute permissionChecks depending on platform
+// centralized permission handling
 export const handlePermissions = () => {
-  if (Platform.OS === 'android') {
-    const readStorage = PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE;
-    const writeStorage = PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE;
-    handleAndroidPermissions([readStorage, writeStorage]);
-  } else if (Platform.OS === 'ios') {
-    // TODO: add iOS permission checks
+  switch (Platform.OS) {
+    case 'android':
+      const readStorage = PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE;
+      const writeStorage = PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE;
+      handleAndroidPermissions([readStorage, writeStorage]);
+      break;
+    default:
+      console.log('unknown Platform');
+      break;
   }
 };
